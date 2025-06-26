be010dd9b38985d8cfd734e6296fa6d82367cb6c
Monardez Ruarte Lucas <lucashruarte05@gmail.com>
2025-06-26 18:43:22 -0300

    termine de agregar las operaciones y los mensajes en el js. Ejercicio 1 terminado

--- ejercicio1/ej1.js
+++ ejercicio1/ej1.js
@@ -3,3 +3,33 @@ const numero2 = document.getElementById('numero2');
 const operacion = document.getElementById('operacion');
 const calcularBtn = document.getElementById('calcular');
 const resultado = document.getElementById('resultado');
+
+operacion.addEventListener('change', function () {
+  if (operacion.value === 'dividir') {
+    calcularBtn.disabled = true;
+  } else {
+    calcularBtn.disabled = false;
+  }
+});
+
+calcularBtn.addEventListener('click', function () {
+  const valor1 = parseFloat(numero1.value);
+  const valor2 = parseFloat(numero2.value);
+  let res = '';
+
+  switch (operacion.value) {
+    case 'sumar':
+      res = valor1 + valor2;
+      break;
+    case 'restar':
+      res = valor1 - valor2;
+      break;
+    case 'multiplicar':
+      res = valor1 * valor2;
+      break;
+    default:
+      res = 'Operación no válida';
+  }
+
+  resultado.textContent = res;
+});
