d0430430e8c76dbace4b7dc223dee749354081c7
Monardez Ruarte Lucas <lucashruarte05@gmail.com>
2025-06-26 20:26:42 -0300

    agrego useEffect para ingresar a la appi y fetch para usar el servidor

--- ejercicio3/src/App.jsx
+++ ejercicio3/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [tareas, setTareas] = useState([]);
-
+  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(res => res.json())
@@ -17,17 +17,4 @@ function App() {
       })
       .catch(error => console.error('Error al obtener tareas:', error));
   }, []);
-
-  return (
-    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-      <h1>Tareas Completadas</h1>
-      <ul>
-        {tareas.map(tarea => (
-          <li key={tarea.id}>{tarea.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
 }
-
-export default App;
