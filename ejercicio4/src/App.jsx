0788fb674136aeb88f941addd1e0a74a93f551dc
Monardez Ruarte Lucas <lucashruarte05@gmail.com>
2025-06-26 21:09:12 -0300

    importe react y useState para manejar estado

---
+++ ejercicio4/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function BotonesAlternados() {
+  const [habilitado, setHabilitado] = useState('izquierdo');
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '400px' }}>
+      <button
+        onClick={() => setHabilitado('derecho')}
+        disabled={habilitado !== 'izquierdo'}
+      >
+        Izquierdo
+      </button>
+
+      <button
+        onClick={() => setHabilitado('izquierdo')}
+        disabled={habilitado !== 'derecho'}
+        style={{ marginLeft: '50px' }}
+      >
+        Derecho
+      </button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BotonesAlternados />);
+
+export default App
