f59d532181a958288a79063f1402093c1559e7b6
Monardez Ruarte Lucas <lucashruarte05@gmail.com>
2025-06-30 21:42:53 -0300

    Mostramos cada tarea completada, ejercicio 3 corregido

--- ejercicio3/ej3.js
+++ ejercicio3/ej3.js
@@ -3,5 +3,16 @@ window.addEventListener('DOMContentLoaded', () => {
     .then(res => res.json())
     .then(data => {
    const completadas = data.filter(tarea => tarea.completed === true); 
+
+      const lista = document.getElementById('lista');
+      
+      completadas.forEach(tarea => {
+        const li = document.createElement('li');
+        li.textContent = `✅ Tarea completada #${tarea.id} del usuario ${tarea.userId}`;
+        lista.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error('Error al obtener las tareas:', error);
     });
 });
